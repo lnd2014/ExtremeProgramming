92bb22080c05c0c9612d7cb20b3fd11715e81e3f
lnd2014 <lndabc@163.com>
2024-11-23 10:20:50 +0800

    初始化项目

---
+++ contacts-back-end/src/main/java/com/example/contactsbackend/ContactsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.contactsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsBackEndApplication.class, args);
+	}
+
+}
